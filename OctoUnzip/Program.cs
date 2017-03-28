4ae1dd4855d1ad9e31df6c10a1368296335265ef
Mark Rydstrom <mark.rydstrom@octopus.com>
2017-03-28 16:01:29 +1000

    Add project files.

---
+++ OctoUnzip/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctoUnzip
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("This program takes a single arguement, the path to a zip file which will be extract to the current directory.");
+                return -1;
+            }
+
+            var path = args[0];
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"Can't find a file at {path}.");
+                return -2;
+            }
+
+            var extractor = new ZipPackageExtractor();
+            var filesExtracted = extractor.Extract(path, Environment.CurrentDirectory);
+
+            Console.WriteLine($"Extracted {filesExtracted} files.");
+
+            return 0;
+        }
+    }
+}
---
+++ OctoUnzip/ZipPackageExtractor.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+
+namespace OctoUnzip
+{
+    public class ZipPackageExtractor
+    {
+        public int Extract(string packageFile, string directory)
+        {
+            var filesExtracted = 0;
+            using (var inStream = new FileStream(packageFile, FileMode.Open, FileAccess.Read))
+            using (var archive = ZipArchive.Open(inStream))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    ProcessEvent(ref filesExtracted, entry);
+                    entry.WriteToDirectory(directory, new ExtractionOptions { ExtractFullPath = true, Overwrite = true, PreserveFileTime = true });
+                }
+            }
+            return filesExtracted;
+        }
+
+        private void ProcessEvent(ref int filesExtracted, IEntry entry)
+        {
+            if (entry.IsDirectory) return;
+
+            filesExtracted++;
+        }
+    }
+}
